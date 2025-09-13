7abeb9c9420d615fb349c62e27ad43b285f66366
Gilbert Donell Chen <carlmabfredishere@gmail.com>
2025-09-14 00:05:02 +0700

    feat: Add task list and add styling

--- app/src/main/java/com/example/to_do_list_app/MainActivity.kt
+++ app/src/main/java/com/example/to_do_list_app/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -20,6 +22,11 @@ import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.material3.*
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
@@ -28,7 +35,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Todo_List_AppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                Scaffold(modifier = Modifier.fillMaxSize(), containerColor = Color(0xFFFEFFE5)) { innerPadding ->
                     MainScreen(padding = innerPadding)
                 }
             }
@@ -42,7 +49,7 @@ fun MainScreen(padding: PaddingValues) {
     var taskList = remember { mutableStateListOf<String>() }
 
     Column(modifier = Modifier.padding(padding)) {
-        Row(modifier = Modifier.padding(bottom = 10.dp)) {
+        Row(modifier = Modifier.padding(bottom = 30.dp).fillMaxWidth()) {
             TaskInput(
                 task = task,
                 onTaskInputChange = { task = it }
@@ -80,9 +87,27 @@ fun AddTaskButton(onClick: () -> Unit) {
 fun TaskList(taskList: SnapshotStateList<String>) {
     LazyColumn {
         items(taskList.size) { index ->
+            TaskItem(taskText = taskList[index])
+        }
+    }
+}
+
+@Composable
+fun TaskItem(taskText: String) {
+    Box (
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(start = 20.dp, end = 20.dp, top = 8.dp, bottom = 8.dp)
+            .shadow(8.dp, RoundedCornerShape(8.dp))
+    ) {
+        Card(
+            modifier = Modifier.fillMaxWidth(),
+            colors = CardDefaults.cardColors(containerColor = Color.White),
+            shape = RoundedCornerShape(8.dp)
+        ) {
             Text(
-                text = taskList[index],
-                modifier = Modifier.padding(bottom = 10.dp)
+                text = taskText,
+                modifier = Modifier.padding(10.dp)
             )
         }
     }
